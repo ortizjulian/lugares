b2be96c15e4c1e19ef8a63903aa57780e9e37afc
ortizjulian <julian.ortixs@gmail.com>
2024-05-21 19:25:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lugares
+ Reto TalentoB
---
+++ src/test/java/com/pruebaTecnica/lugares/LugaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.lugares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
